513955a371b8450ffecbdd54289cdfc1445596e7
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2019-07-27 16:33:17 -0700

    Add feature for adding manga source to mongodb

--- backend/MangaSourcesFetcher/MangaSourcesFetcher.py
+++ backend/MangaSourcesFetcher/MangaSourcesFetcher.py
@@ -1,1 +1,29 @@
+import pymongo
+import datetime
+import params
 
+def addMangaSource(url, name=None, queryURLTemplate=None):
+    client = pymongo.MongoClient(params.MONGODB_HOST, params.MONGODB_PORT)
+    db = client[params.DB_NAME]
+    manga_sources = db[params.MANGA_SOURCE_COLLECTIONS]
+
+    now = datetime.datetime.utcnow()
+    key = {params.MANGA_SOURCE_URL_FIELD: url}
+    data = {
+         params.MONGODB_SET_ON_INSERT: { 
+             params.MANGA_SOURCE_INSERTION_DATE_FIELD: now 
+             },
+         params.MONGODB_SET: {
+             params.MANGA_SOURCE_NAME_FIELD: name,
+             params.MANGA_SOURCE_QUERYURL_FIELD: queryURLTemplate,
+             params.MANGA_SOURCE_LAST_UPDATE_FIELD: now
+             }
+         }
+
+    result = manga_sources.update(key, data, upsert=True)
+    return result
+
+if __name__ == '__main__':
+    addMangaSource(url='http://fanfox.net/', 
+                   name='Manga Fox', 
+                   queryURLTemplate='http://fanfox.net/manga{/manga_name}')
